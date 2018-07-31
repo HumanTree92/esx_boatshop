b10e45fa9714a319decc830742a29a8743de75e4
William Plunkett Jr <joey.plunkett92@yahoo.com>
2018-07-30 21:58:19 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,18 @@
 Config                            = {}
 Config.DrawDistance               = 100.0
 Config.MarkerColor                = { r = 120, g = 120, b = 240 }
-Config.EnablePlayerManagement     = false -- If set to true, you need esx_addonaccount, esx_billing and esx_society
+Config.EnablePlayerManagement     = false -- enables the actual car dealer job. You'll need esx_addonaccount, esx_billing and esx_society
 Config.EnablePbCommand            = false
 Config.EnableOwnedVehicles        = true
-Config.EnableSocietyOwnedVehicles = false
+Config.EnableSocietyOwnedVehicles = false -- use with EnablePlayerManagement disabled, or else it wont have any effects
 Config.ResellPercentage           = 50
 Config.Locale                     = 'en'
+
+-- looks like this: 'LLL NNN'
+-- The maximum plate length is 8 chars (including spaces & symbols), don't go past it!
+Config.PlateLetters  = 3
+Config.PlateNumbers  = 3
+Config.PlateUseSpace = true
 
 Config.Zones = {
 
